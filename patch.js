741898e3c58f745ae2d73c3dc837044931f653b1
Linfindel <116459177+linfindel@users.noreply.github.com>
2023-12-06 10:07:43 +0000

    Add patch note system

---
+++ patch.js
@@ -0,0 +1,23 @@
+function patchNotes(repo) {
+    const username = 'ChurchOf-Chonk';
+
+    fetch(`https://api.github.com/repos/${username}/${repo}/commits?per_page=1`)
+    .then(response => {
+        const totalCount = response.headers.get('Link').match(/page=(\d+)>; rel="last"/)[1] / 100;
+        return response.json().then(data => {
+            const latestCommitMessage = data[0].commit.message;
+            console.log(`Total commit count: ${totalCount}`);
+            console.log(`Latest commit message: ${latestCommitMessage}`);
+            
+            document.getElementById(`${repo}-version`).innerText = `Version ${totalCount} Patch Notes`;
+            document.getElementById(`${repo}-whats-new`).innerText = latestCommitMessage;
+
+            document.getElementById(`${repo}-about-card`).style.opacity = "1";
+            document.getElementById(`${repo}-about-card`).style.pointerEvents = "all";
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+patchNotes("churchof-chonk.github.io");
+patchNotes("chonkbible");
