82fb3f672f1fd2371b25b4dc7f46982644c2f693
Linfindel <116459177+linfindel@users.noreply.github.com>
2023-12-19 21:53:37 +0000

    Extend animations

--- intro.js
+++ intro.js
@@ -1,10 +1,16 @@
-setTimeout(() => {
-    document.getElementById("dots").style.animation = "dots-final 2.5s ease alternate infinite both";
+if (document.referrer.includes("patch.html")) {
+    showUI();
+}
 
+else {
     setTimeout(() => {
-	    showUI();
-    }, 5000);
-}, 5000);
+        document.getElementById("dots").style.animation = "dots-final 2.5s ease alternate infinite both";
+    
+        setTimeout(() => {
+            showUI();
+        }, 5000);
+    }, 5000);   
+}
 
 function showUI() {
     document.getElementById("dots").style.animation = "dots 5s ease reverse both";
@@ -12,23 +18,54 @@ function showUI() {
     setTimeout(() => {
         document.getElementById("navbar").style.opacity = "1";
         document.getElementById("navbar").style.transform = "translateY(0)";
-
+    
         document.getElementById("patch-button").style.opacity = "1";
         document.getElementById("patch-button").style.transform = "translateX(0)";
-
+    
         document.getElementById("bible-card").style.opacity = "1";
         document.getElementById("bible-card").style.transform = "translateY(0)";
-
+    
         setTimeout(() => {
             document.getElementById("mantra-card").style.opacity = "1";
             document.getElementById("mantra-card").style.transform = "translateY(0)";
         }, 500);
 
         setTimeout(() => {
-            document.getElementById("navbar").style.transition = "0.25s ease";
             document.getElementById("patch-button").style.transition = "0.25s ease";
 
-            document.body.style.overflow = "initial";
+            document.body.style.pointerEvents = "all";
         }, 2500);
+    }, 5000);
+}
+
+function exitToPatch() {
+    document.getElementById("navbar").style.transition = "2.5s ease";
+    document.getElementById("patch-button").style.transition = "2.5s ease";
+
+    document.body.style.overflow = "hidden";
+
+
+
+    document.getElementById("navbar").style.opacity = "0";
+    document.getElementById("navbar").style.transform = "translateY(-10rem)";
+
+    document.getElementById("patch-button").style.opacity = "0";
+    document.getElementById("patch-button").style.transform = "translateX(-10rem)";
+
+    document.getElementById("mantra-card").style.opacity = "0";
+    document.getElementById("mantra-card").style.transform = "translateY(5rem)";
+
+    document.getElementById("bible-card").style.opacity = "0";
+    document.getElementById("bible-card").style.transform = "translateY(5rem)";
+
+    document.getElementById("dots").style.animation = "dots-final 2.5s ease alternate infinite both"; 
+    document.getElementById("dots").style.opacity = "0";
+
+    setTimeout(() => {
+        document.getElementById("dots").style.opacity = "1";
+    }, 2500);
+
+    setTimeout(() => {
+        location.href = 'patch.html'; 
     }, 5000);
 }
--- patch.js
+++ patch.js
@@ -12,7 +12,6 @@ function patchNotes(repo) {
             document.getElementById(`${repo}-version`).innerText = `Version ${totalCount} Patch Notes`;
             document.getElementById(`${repo}-whats-new`).innerText = latestCommitMessage;
 
-            document.getElementById(`${repo}-about-card`).style.opacity = "1";
             document.getElementById(`${repo}-about-card`).style.pointerEvents = "all";
         });
     })
@@ -22,3 +21,89 @@ function patchNotes(repo) {
 patchNotes("churchof-chonk.github.io");
 patchNotes("chonkbible");
 patchNotes("mantra");
+
+/// Intro
+document.getElementById("dots").style.animation = "dots 5s ease reverse both";
+
+setTimeout(() => {
+    document.getElementById("navbar").style.opacity = "1";
+    document.getElementById("navbar").style.transform = "translateY(0rem)";
+
+    document.getElementById("back-button").style.opacity = "1";
+    document.getElementById("back-button").style.transform = "translateX(0rem)";
+
+    document.getElementById("churchof-chonk.github.io-about-card").style.opacity = "1";
+    document.getElementById("churchof-chonk.github.io-about-card").style.transform = "translateY(0rem)";
+
+    setTimeout(() => {
+        document.getElementById("chonkbible-about-card").style.opacity = "1";
+        document.getElementById("chonkbible-about-card").style.transform = "translateY(0rem)";
+    }, 500);
+
+    setTimeout(() => {
+        document.getElementById("mantra-about-card").style.opacity = "1";
+        document.getElementById("mantra-about-card").style.transform = "translateY(0rem)";
+    }, 1000);
+
+    setTimeout(() => {
+        document.getElementById("churchof-chonk.github.io-about-card").style.transition = "0.25s ease";
+
+        document.getElementById("back-button").style.transition = "0.25s ease";
+        document.body.style.pointerEvents = "all";
+    }, 2500);
+    
+    setTimeout(() => {
+        document.getElementById("chonkbible-about-card").style.transition = "0.25s ease";
+    }, 3000);
+
+    setTimeout(() => {
+        document.getElementById("mantra-about-card").style.transition = "0.25s ease";
+
+        document.getElementById("navbar").style.transition = "none";
+        document.getElementById("navbar").style.width = "100%";
+
+        document.getElementById("column").style.transition = "none";
+        document.getElementById("column").style.marginRight = "0";
+
+        document.body.style.overflow = "initial";
+    }, 3500);
+}, 5000);
+
+function exitToHome() {
+    document.getElementById("navbar").style.transition = "2.5s ease";
+
+    document.body.style.overflow = "hidden";
+
+    document.getElementById("column").style.transition = "none";
+    document.getElementById("column").style.marginRight = "1rem";
+
+    document.getElementById("churchof-chonk.github.io-about-card").style.transition = "2.5s ease";
+    document.getElementById("chonkbible-about-card").style.transition = "2.5s ease";
+    document.getElementById("mantra-about-card").style.transition = "2.5s ease";
+
+    document.getElementById("navbar").style.opacity = "0";
+    document.getElementById("navbar").style.transform = "translateY(-10rem)";
+
+    document.getElementById("back-button").style.opacity = "0";
+    document.getElementById("back-button").style.transform = "translateX(-10rem)";
+
+    document.getElementById("churchof-chonk.github.io-about-card").style.opacity = "0";
+    document.getElementById("churchof-chonk.github.io-about-card").style.transform = "translateY(5rem)";
+
+    document.getElementById("chonkbible-about-card").style.opacity = "0";
+    document.getElementById("chonkbible-about-card").style.transform = "translateY(5rem)";
+
+    document.getElementById("mantra-about-card").style.opacity = "0";
+    document.getElementById("mantra-about-card").style.transform = "translateY(5rem)";
+
+    document.getElementById("dots").style.animation = "dots-final 2.5s ease alternate infinite both"; 
+    document.getElementById("dots").style.opacity = "0";
+
+    setTimeout(() => {
+        document.getElementById("dots").style.opacity = "1";
+    }, 2500);
+
+    setTimeout(() => {
+        location.href = 'index.html'
+    }, 5000);
+}
